9331cc320e8ca78b33c41aa64b57a6fc721062cc
ptvprasad <163627197+ptvprasad@users.noreply.github.com>
2024-08-21 10:14:29 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -11,10 +11,10 @@ sudo apt install -y
 
 # to install jenkins import the jenkins to apt repo
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
-curl -fsSL https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+ https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 # installing jenkins
