5572f785eb85f93455459b323eae263c803bfd42
ptvprasad <163627197+ptvprasad@users.noreply.github.com>
2024-12-21 21:46:01 +0530

    Create jenk.sh

---
+++ jenk.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#updating
+
+sudo apt update -y
+
+# installing java 
+sudo apt install openjdk-17-jdk -y
+
+# to install jenkins import the jenkins to apt repo
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+ https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# installing jenkins
+sudo apt update -y
+sudo apt install jenkins -y
+
+# open jenkins password
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
